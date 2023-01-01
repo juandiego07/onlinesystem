560fdb82bb84c59215bc12fc9fb8fb811017f96b
juandiego07 <juan_diego07@hotmail.com>
2023-01-01 00:32:25 -0500

    Add HomeController and I created filter functionality

--- resources/views/epayco/dashboard.blade.php
+++ resources/views/epayco/dashboard.blade.php
@@ -5,101 +5,52 @@
 @section('main')
     <div class="row mtop">
         <div class="d-flex flex-wrap flex-fill justify-content-center">
-            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-                <label class="btn btn-outline-dark" for="btnradio1">Todas</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-                <label class="btn btn-outline-dark" for="btnradio2">Anuladas</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-                <label class="btn btn-outline-dark" for="btnradio3">Pagadas</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
-                <label class="btn btn-outline-dark" for="btnradio4">Pendientes</label>
+            <div class="btn-group">
+                <a href={{ route('dashboard') }} class="btn btn-outline-dark active filter" aria-current="page">Todas</a>
+                <a href={{ route('dashboard.show', ['status' => 'Anulada']) }}
+                    class="btn btn-outline-danger filter">Anulada</a>
+                <a href={{ route('dashboard.show', ['status' => 'Pagada']) }}
+                    class="btn btn-outline-success filter">Pagada</a>
+                <a href={{ route('dashboard.show', ['status' => 'Pendiente']) }}
+                    class="btn btn-outline-warning filter">Pendiente</a>
             </div>
         </div>
     </div>
+
+    @isset($Anulada)
+        <script>
+            filter('Anulada')
+        </script>
+    @endisset
+    @isset($Pagada)
+        <script>
+            filter('Pagada')
+        </script>
+    @endisset
+    @isset($Pendiente)
+        <script>
+            filter('Pendiente')
+        </script>
+    @endisset
+
     <div class="row">
         <div class="d-flex flex-wrap flex-fill justify-content-center">
-            <div class="card card-style m-2 border-success">
-                <div class="row g-0">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <h5 class="card-title">N° de factura 001</h5>
-                            <p class="card-text">Descripción: Servicios ePayco Diciembre</p>
-                            <p class="card-text">Cliente: Juan Diego Vargas Posada</p>
-                            <p class="card-text"><small class="text-muted">27/12/2022</small></p>
+            @foreach ($bills as $bill)
+                <div class="card card-style m-2 border" id="{{ $bill->status }}">
+                    <div class="row g-0">
+                        <div class="col-md-12">
+                            <div class="card-body">
+                                <h5 class="card-title">N° de factura {{ $bill->id }}</h5>
+                                <p class="card-text">Descripción: {{ $bill->description }}</p>
+                                <p class="card-text">Cliente: {{ $bill->name . ' ' . $bill->last_name }}</p>
+                                <p class="card-text"><small class="text-muted">Fecha de vencimiento
+                                        {{ $bill->expiration_date }}</small></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="card card-style m-2 border-success">
-                <div class="row g-0">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <h5 class="card-title">N° de factura 002</h5>
-                            <p class="card-text">Descripción: Servicios ePayco Diciembre</p>
-                            <p class="card-text">Cliente: Juan Diego Vargas Posada</p>
-                            <p class="card-text"><small class="text-muted">27/12/2022</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card card-style m-2 border-warning">
-                <div class="row g-0">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <h5 class="card-title">N° de factura 003</h5>
-                            <p class="card-text">Descripción: Servicios ePayco Diciembre</p>
-                            <p class="card-text">Cliente: Juan Diego Vargas Posada</p>
-                            <p class="card-text"><small class="text-muted">27/12/2022</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card card-style m-2 border-danger">
-                <div class="row g-0">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <h5 class="card-title">N° de factura 004</h5>
-                            <p class="card-text">Descripción: Servicios ePayco Diciembre</p>
-                            <p class="card-text">Cliente: Juan Diego Vargas Posada</p>
-                            <p class="card-text"><small class="text-muted">27/12/2022</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card card-style m-2 border-danger">
-                <div class="row g-0">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <h5 class="card-title">N° de factura 004</h5>
-                            <p class="card-text">Descripción: Servicios ePayco Diciembre</p>
-                            <p class="card-text">Cliente: Juan Diego Vargas Posada</p>
-                            <p class="card-text"><small class="text-muted">27/12/2022</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card card-style m-2 border-danger">
-                <div class="row g-0">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <h5 class="card-title">N° de factura 004</h5>
-                            <p class="card-text">Descripción: Servicios ePayco Diciembre</p>
-                            <p class="card-text">Cliente: Juan Diego Vargas Posada</p>
-                            <p class="card-text"><small class="text-muted">27/12/2022</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            @endforeach
+            <script>border()</script>
         </div>
     </div>
 
